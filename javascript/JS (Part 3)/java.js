3b11a8c47c2056ddd57daeb560d39348f437f8d6
jawad qasim <jawadqasim99@gmail.com>
2024-01-19 21:22:24 +0500

    understanding java concept

--- javascript/JS (Part 3)/java.js
+++ javascript/JS (Part 3)/java.js
@@ -33,12 +33,44 @@
     
 // }
 
-let array = [["Jawad " , "Qasim" ,"Saim"],["Ali" , "zain" , "Kahsif"]];
-for (let i = 0; i < array.length; i++) {
-    console.log(`List# ${i}`);
-    for (let j = 0; j < array[i].length; j++) {
-       console.log(array[i][j]);
+// let array = [["Jawad " , "Qasim" ,"Saim"],["Ali" , "zain" , "Kahsif"]];
+// for (let i = 0; i < array.length; i++) {
+//     console.log(`List# ${i}`);
+//     for (let j = 0; j < array[i].length; j++) {
+//        console.log(array[i][j]);
         
-    }
+//     }
     
+// }
+
+// let arr = [1,2,3,4,5,6,2,3];
+// for (let i = 0; i < arr.length; i++) {
+//    if(arr[i] == 2){
+//     arr.splice(i,1);
+//    }
+    
+// }
+// console.log(arr);
+
+// let number = 287152;
+// let count = 0;
+// let copy = number;
+// while(copy > 0) {
+// count++;
+// copy = Math.floor(copy/10);
+
+// }
+// console.log(count);
+
+let number = 123543;
+let sum = 0;
+let copy=number;
+
+while(copy > 0){
+digit= copy%10;
+sum += digit;
+copy=Math.floor(copy/10);
 }
+console.log(sum);
+
+   
