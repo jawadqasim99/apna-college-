3a01e665544c5c32414f6fb03b5defba300c5d5d
jawad qasim <jawadqasim99@gmail.com>
2024-01-21 20:55:27 +0500

    today solve dom

---
+++ DOM project/JS DOM/part10/java.js
@@ -0,0 +1,30 @@
+// let div=document.querySelector(".container");
+// div.addEventListener("scroll" , ()=>{
+
+// } )
+// div.scroll(0,1000);
+
+
+// mail.addEventListener("keypress",(e)=>{
+// h1.innerText=[e.key];
+// })
+// let btn=document.querySelector("button");
+// btn.addEventListener("click" , ()=>{
+//     input_value;
+// })
+
+// function input_value() {
+//     let mail=document.querySelector("#email");
+//     let h1=document.querySelector("h1");
+//     let replace_value=mail.value.replace(/[^a-zA-Z]/g, " ");
+
+// h1.innerText=replace_value;
+
+    
+}
+
+// 
+// h1.scroll(0,10000);
+// addEventListener("load", (event) => {
+//     alert("page is fully loaded");
+//   });
--- DOM project/java.js
+++ DOM project/java.js
@@ -1,24 +1,43 @@
-let para=document.createElement('p');
+// let para=document.createElement('p');
 
-para.innerText="Hey i'am read";
-para.classList.add("para");
-document.querySelector("body").append(para);
+// para.innerText="Hey i'am read";
+// para.classList.add("para");
+// document.querySelector("body").append(para);
 
-let heading=document.createElement("h3");
-heading.innerText="I am blue";
-heading.classList.add('heading')
-document.querySelector("body").append(heading);
+// let heading=document.createElement("h3");
+// heading.innerText="I am blue";
+// heading.classList.add('heading')
+// document.querySelector("body").append(heading);
 
-let div=document.createElement("div");
-let h=document.createElement("h1");
-let p=document.createElement("p");
-h.innerText="my name is DIV";
-p.innerText="para is div";
-div.append(h);
-div.append(p);
-div.classList.add("container");
-document.querySelector("body").append(div);
-
-
+// let div=document.createElement("div");
+// let h=document.createElement("h1");
+// let p=document.createElement("p");
+// h.innerText="my name is DIV";
+// p.innerText="para is div";
+// div.append(h);
+// div.append(p);
+// div.classList.add("container");
+// document.querySelector("body").append(div);
 
 
+
+
+let btn = document.querySelector("button");
+btn.addEventListener("click" , ()=>{
+    let value=colors();
+   let h1=document.querySelector("h1");
+   h1.innerText=value;
+   h1.style.color=value;
+   let div=document.querySelector("div");
+   div.style.backgroundColor=value;
+   btn.style.backgroundColor=value;
+})
+
+let colors=()=>{
+    let red=Math.floor(Math.random() * 255);
+    let green=Math.floor(Math.random() * 255);
+    let blue=Math.floor(Math.random() * 255);
+
+    let color =`rgb(${red} , ${green} , ${blue})`;
+    return color;
+}
