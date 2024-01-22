55bc960d5cec3975ae8283a341135026b4483cba
jawad qasim <jawadqasim99@gmail.com>
2024-01-22 21:06:09 +0500

    Understanding a promise and promise chaning and then&cath

--- DOM project/js call stack/java.js
+++ DOM project/js call stack/java.js
@@ -24,22 +24,50 @@
 
 // console.log("jawad");
 
-let h1=document.querySelector("h1");
+// let h1=document.querySelector("h1");
 
-function chnageColors(colors,time,callbacks) {
-    setTimeout(()=>{
-        h1.style.color=colors;
-        if(callbacks) callbacks();
-    },time)
+// function chnageColors(colors,time,callbacks) {
+//     setTimeout(()=>{
+//         h1.style.color=colors;
+//         if(callbacks) callbacks();
+//     },time)
+    
+// }
+
+// chnageColors("red",1000,()=>{
+//     chnageColors("green",1000,()=>{
+//         chnageColors("yellow",1000,()=>{
+//             chnageColors("cyan",1000);
+//         });
+//     });
+// });
+
+
+function interSpeed(data) {
+    return new Promise((resolve , reject)=>{
+        let speed=Math.floor(Math.random() * 10)+1;
+    if(speed > 4){
+resolve("Succcessfully!!");
+    }else{
+        reject("internet Speed poor!!");
+    }
+    });
+    
     
 }
 
-chnageColors("red",1000,()=>{
-    chnageColors("green",1000,()=>{
-        chnageColors("yellow",1000);
-    });
-});
-
-// chnageColors("yellow",3000);
-// chnageColors("cyan",4000);
+let request=interSpeed("apna college"); 
+request.then((res)=>{
+    console.log(res);
+console.log("Successfully upload!!!");
+return interSpeed("apna");
+})
+.then((res)=>{
+    console.log(res);
+    console.log("snd fill");
+})
+.catch((error)=>{
+    console.log(error);
+    console.log("Server error");
+})
 
