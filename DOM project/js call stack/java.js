a0aa1e02e4a885f819e8e2f059fb1d067068e326
jawad qasim <jawadqasim99@gmail.com>
2024-01-23 10:11:04 +0500

    wo understanding the concept of async it create a promise object using async and await we resolve the problem a call back hell and promise long mathod then and catch

--- DOM project/js call stack/java.js
+++ DOM project/js call stack/java.js
@@ -43,31 +43,65 @@
 // });
 
 
-function interSpeed(data) {
-    return new Promise((resolve , reject)=>{
-        let speed=Math.floor(Math.random() * 10)+1;
-    if(speed > 4){
-resolve("Succcessfully!!");
-    }else{
-        reject("internet Speed poor!!");
-    }
-    });
+// function interSpeed(data) {
+//     return new Promise((resolve , reject)=>{
+//         let speed=Math.floor(Math.random() * 10)+1;
+//     if(speed > 4){
+// resolve("Succcessfully!!");
+//     }else{
+//         reject("internet Speed poor!!");
+//     }
+//     });
     
+    
+// }
+
+// let request=interSpeed("apna college"); 
+// request.then((res)=>{
+//     console.log(res);
+// console.log("Successfully upload!!!");
+// return interSpeed("apna");
+// })
+// .then((res)=>{
+//     console.log(res);
+//     console.log("snd fill");
+// })
+// .catch((error)=>{
+//     console.log(error);
+//     console.log("Server error");
+// })
+
+// let h1=document.querySelector("h1");
+
+// function chnageColors(colors,time,callbacks) {
+//     setTimeout(()=>{
+//         h1.style.color=colors;
+//         if(callbacks) callbacks();
+//     },time)
+    
+// }
+
+// chnageColors("red",1000,()=>{
+//     chnageColors("green",1000,()=>{
+//         chnageColors("yellow",1000,()=>{
+//             chnageColors("cyan",1000);
+//         });
+//     });
+// });
+
+function  changeColorsPromise(colors,delay) {
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            h1.style.color=colors;
+            resolve("Colors is changed") 
+        },delay)
+    })
     
 }
 
-let request=interSpeed("apna college"); 
-request.then((res)=>{
-    console.log(res);
-console.log("Successfully upload!!!");
-return interSpeed("apna");
-})
-.then((res)=>{
-    console.log(res);
-    console.log("snd fill");
-})
-.catch((error)=>{
-    console.log(error);
-    console.log("Server error");
-})
-
+async function demo(){
+    await changeColorsPromise("red" , 1000);
+    await changeColorsPromise("green" , 1000);
+    await changeColorsPromise("blue" , 1000);
+     changeColorsPromise("orange" , 1000);
+}
