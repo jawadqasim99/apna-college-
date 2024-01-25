5c4828016e8c2eea4d3c34b30b7cd7b48e9e9d46
jawad qasim <jawadqasim99@gmail.com>
2024-01-25 13:10:15 +0500

    Today leran ejs tamplet it is very use full when fetch data from data base and list out data it use use to make a dynamic html page just like instagram page is used to make dynamic website

---
+++ backend developmet/EJS/index.js
@@ -0,0 +1,47 @@
+let express = require("express");
+const path = require("path");
+const app = express();
+
+// console.log();
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.listen(3000,(req , res)=>{
+    console.log("Server Started");
+})
+
+app.get("/",(req,res)=>{
+ res.render("home");
+})
+
+app.get("/rolldice",(req,res)=>{
+    let rolldice=Math.floor(Math.random() *6) + 1;
+    res.render("rolldice.ejs",{rolldice});
+   })
+
+//    
+// app.get("/id/:name",(req,res)=>{
+    //    let {name}=req.params;
+    // //    console.log(name);
+    //     res.render("instagram.ejs",{names:name});
+    //    })
+
+    // app.get("/:username",(req,res)=>{
+    //        let {username}=req.params;
+    //        let dataValue=require("./data.json");
+    //        let data=dataValue[username];
+    //        console.log(data);   
+        
+    //         res.render("instagram.ejs");
+    //        });
+
+    app.get("/id/:username" , (req , res)=>{
+        let {username}=req.params;
+        const instData=require("./data.json");
+        const data=instData[username];
+      if(data){
+        res.render("instagram.ejs" , {data});
+      }else{
+        res.render("error.ejs" );
+      }
+    })
